b2957c57a04d22864990c441723eb95532d75add
Nithin Kumar <nithin_22b226cs@gecwyd.ac.in>
2026-09-10 20:43:45 +0530

    Server Setuped

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,23 @@
-const { app, connectDB } = require('./app');
+const express=require('express');
+const cors=require('cors');
 
-const PORT = process.env.PORT || 5000;
+const app=express();
+app.use(cors());
+app.use(express.json());
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error('MongoDB connection error:', err));
+const mongoose=require('mongoose');
+
+mongoose.connect('process.env.MONGO_URI')
+.then(()=>console.log('MongoDB connected'))
+.catch(err=>console.log(err));
+
+
+app.get('/',(req,res)=>{
+    res.send('API is running...');
+});
+
+const authRoutes=require('./routes/authRoutes');
+app.use('/api/auth',authRoutes);
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
