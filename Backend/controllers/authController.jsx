e9659b3558dcf7738d846041e0a460eb985c5f3d
Nithin Kumar <nithin_22b226cs@gecwyd.ac.in>
2026-09-11 21:48:24 +0530

    login Endpoint Completed

--- Backend/controllers/authController.jsx
+++ Backend/controllers/authController.jsx
@@ -1,9 +1,9 @@
-const user=require('../models/userModel');
+const User=require('../models/userModel');
 const bcrypt=require('bcryptjs');
 const jwt=require('jsonwebtoken');
 
 
-exports.signUp=(req,res)=>{
+exports.signUp=async (req,res)=>{
     try{
     const {displayname,email,password} = req.body;
     if(!displayname || !email || !password){
@@ -13,7 +13,7 @@ exports.signUp=(req,res)=>{
     if(password.length<6){
         return res.status(400).json({message:'password Should be atleast 6 characters long'});
     }
-    const existingUser=user.findOne({email});
+    const existingUser=await User.findOne({email});
     if(existingUser){
         return res.status(400).json({message:'User already Exists'});
     }
@@ -21,17 +21,33 @@ exports.signUp=(req,res)=>{
     const hashedPassword=bcrypt.hashSync(password,10);
     
 
-    const User=await user.Create({
+    const user=await User.create({
         displayname,
         email,
         password:hashedPassword,
     })
-    res.json({message:'User Created Successfully',User});
+    res.json({message:'User Created Successfully'});
     }catch(err){
     res.status(500).json({message:'Server Error',err:err.message})
 }
-
-
-
 };
-exports.login=(req,res)=>{};
+exports.login=async (req,res)=>{
+    try{
+    const {email,password}=req.body;
+     if(!email || !password){
+        return res.status(400).json({message:'Please fill all the fields'});
+    }
+    const existingUser=await User.findOne({email});
+    if(!existingUser){
+        return res.status(400).json({message:'Invalid Credentials'});
+    }
+    const isMatch=bcrypt.compareSync(password,existingUser.password);
+    if(!isMatch){
+        return res.status(400).json({message:'Invalid Credentials'});
+    }
+    const token=jwt.sign({id:existingUser._id,email:email},process.env.JWT_SECRET,{expiresIn:'1h'});
+    res.json({message:'Login Successful', token});
+}catch(err){
+    res.status(500).json({message:'Server Error', err:err.message})
+}
+};
